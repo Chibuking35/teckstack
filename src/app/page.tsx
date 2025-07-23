67446310fe3b1932773fae094b5bf3c9993ffa0f
Chibuking32 <cnwigwe525@gmail.com>
2025-07-23 12:01:34 -0700

    modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
 
-const Home =()=> {
+const Home = () => {
   return (
     <div className="p-5">
-    <h1 className="text-2xl font-bold mb-4">Welcome to the Teck Stack!</h1>
-     hello, Gloria it's chibueze. you can now give me a ui/ux for the website.
+      <h1 className="text-2xl font-bold mb-4">Welcome to the Teck Stack!</h1>
 
-    <p className="mb-4 font-bold">Trust you have made your reseach about it.</p>
-     <Image src="/techstack.png" alt="" width={400} height={70} className="p-2"></Image>
+      <p className="mb-4">hello, Gloria it&apos;s chibueze. you can now give me a UI/UX for the website.</p>
+
+      <p className="mb-4 font-bold">Trust you have made your research about it.</p>
+
+      <Image src="/techstack.png" alt="Tech Stack" width={400} height={70} className="p-2" />
     </div>
   )
 }
+
 export default Home
