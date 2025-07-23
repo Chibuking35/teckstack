57ceffaebe07bc09459578277efc5481788db3a2
Chibuking32 <cnwigwe525@gmail.com>
2025-07-23 12:09:57 -0700

    Fix ESLint error: escaped apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ const Home = () => {
     <div className="p-5">
       <h1 className="text-2xl font-bold mb-4">Welcome to the Teck Stack!</h1>
 
-      <p className="mb-4">hello, Gloria it&apos;s chibueze. you can now give me a UI/UX for the website.</p>
+      <p className="mb-4">
+        hello, Gloria it&apos;s chibueze. you can now give me a UI/UX for the website.
+      </p>
 
       <p className="mb-4 font-bold">Trust you have made your research about it.</p>
 
